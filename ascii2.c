423641252a18b76652d2fdbd4dda8544cd6f7d32
jucafu1819daw2 <44981518+jucafu1819daw2@users.noreply.github.com>
2019-06-04 21:02:34 +0200

    Commit 1 del projecte ascii2 a la branca ascii2_ampliada

---
+++ ascii2.c
@@ -0,0 +1,21 @@
+//Autor: Juan Jose Castillo Funes
+//Data: 18-2-19
+//Programa: ascii3
+//VesiÃ³: 1.0
+//
+#include <stdio.h>
+
+int main() {
+	char cCar1,cCar2;
+	int n,i;
+		
+	printf("TAULA ASCII 4-6-2019\n");
+	printf("CAR  HEX  DEC   CAR  HEX  DEC\n");
+	n=('Z'-'A')/2;
+	for (i=0;i<=n;i++) {
+		cCar1='A'+i;
+		cCar2='A'+i+1+n;
+		printf("%c    %x   %d    %c    %x   %d\n",cCar1+32,cCar1+32,cCar1+32,cCar2+32,cCar2+32,cCar2+32);		
+	}
+	return(0);
+}
